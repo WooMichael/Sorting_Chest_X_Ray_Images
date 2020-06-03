4c020be8b06ff818e2a0f32b8340c59f4f5afac2
Michael Woo <michaelwoo120@gmail.com>
2020-06-02 21:08:12 -0400

    Worked on Test Files
    Files on the test folder were separated into their folders

--- X-ray_Seperator_for_train_data.py
+++ X-ray_Seperator_for_train_data.py
@@ -13,25 +13,25 @@ while i < len(image_name):
 for pup in tri_tuple:
     if (pup.__getitem__(2) == 'TRAIN' and pup.__getitem__(1) == 'Normal  '):
         copyfile('Coronahack-Chest-XRay-Dataset/Coronahack-Chest-XRay-Dataset/train/' + pup.__getitem__(0),
-                 'Coronahack-Chest-XRay-Dataset/Train/Normal/' + pup.__getitem__(0))
+                 'Coronahack-Chest-XRay-Dataset/train_data_seperated_into_individual_folders/Normal/' + pup.__getitem__(0))
     if (pup.__getitem__(2) == 'TRAIN' and pup.__getitem__(1) == 'Pnemonia  bacteria'):
         copyfile('Coronahack-Chest-XRay-Dataset/Coronahack-Chest-XRay-Dataset/train/' + pup.__getitem__(0),
-                 'Coronahack-Chest-XRay-Dataset/Train/Pnemonia bacteria/' + pup.__getitem__(0))
+                 'Coronahack-Chest-XRay-Dataset/train_data_seperated_into_individual_folders/Pnemonia bacteria/' + pup.__getitem__(0))
     if (pup.__getitem__(2) == 'TRAIN' and pup.__getitem__(1) == 'Pnemonia Streptococcus bacteria'):
         copyfile('Coronahack-Chest-XRay-Dataset/Coronahack-Chest-XRay-Dataset/train/' + pup.__getitem__(0),
-                 'Coronahack-Chest-XRay-Dataset/Train/Pnemonia bacteria Streptococcus/' + pup.__getitem__(0))
+                 'Coronahack-Chest-XRay-Dataset/train_data_seperated_into_individual_folders/Pnemonia bacteria Streptococcus/' + pup.__getitem__(0))
     if (pup.__getitem__(2) == 'TRAIN' and pup.__getitem__(1) == 'Pnemonia ARDS Stress-Smoking'):
         copyfile('Coronahack-Chest-XRay-Dataset/Coronahack-Chest-XRay-Dataset/train/' + pup.__getitem__(0),
-                 'Coronahack-Chest-XRay-Dataset/Train/Pnemonia Stress-Smoking ARDS/' + pup.__getitem__(0))
+                 'Coronahack-Chest-XRay-Dataset/train_data_seperated_into_individual_folders/Pnemonia Stress-Smoking ARDS/' + pup.__getitem__(0))
     if (pup.__getitem__(2) == 'TRAIN' and pup.__getitem__(1) == 'Pnemonia  Virus'):
         copyfile('Coronahack-Chest-XRay-Dataset/Coronahack-Chest-XRay-Dataset/train/' + pup.__getitem__(0),
-                 'Coronahack-Chest-XRay-Dataset/Train/Pnemonia Virus/' + pup.__getitem__(0))
+                 'Coronahack-Chest-XRay-Dataset/train_data_seperated_into_individual_folders/Pnemonia Virus/' + pup.__getitem__(0))
     if (pup.__getitem__(2) == 'TRAIN' and pup.__getitem__(1) == 'Pnemonia COVID-19 Virus'):
         copyfile('Coronahack-Chest-XRay-Dataset/Coronahack-Chest-XRay-Dataset/train/' + pup.__getitem__(0),
-                 'Coronahack-Chest-XRay-Dataset/Train/Pnemonia Virus COVID-19/' + pup.__getitem__(0))
+                 'Coronahack-Chest-XRay-Dataset/train_data_seperated_into_individual_folders/Pnemonia Virus COVID-19/' + pup.__getitem__(0))
     if (pup.__getitem__(2) == 'TRAIN' and pup.__getitem__(1) == 'Pnemonia SARS Virus'):
         copyfile('Coronahack-Chest-XRay-Dataset/Coronahack-Chest-XRay-Dataset/train/' + pup.__getitem__(0),
-                 'Coronahack-Chest-XRay-Dataset/Train/Pnemonia Virus SARS/' + pup.__getitem__(0))
+                 'Coronahack-Chest-XRay-Dataset/train_data_seperated_into_individual_folders/Pnemonia Virus SARS/' + pup.__getitem__(0))
 # print(label)
 # print(image_name)
 # print(class_type)
